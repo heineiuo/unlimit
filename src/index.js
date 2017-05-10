177029f7bf80007cb3b811a968aae010bcec8e5d
heineiuo <heineiuo@gmail.com>
2017-05-10 15:38:01 +0800

    Move system set to src/index.js

--- src/index.js
+++ src/index.js
@@ -6,6 +6,12 @@ window.ReactRouterDOM = ReactRouterDOM
 window.Redux = Redux
 window.ReactRedux = ReactRedux
 
+SystemJSJS.set(SystemJS.normalizeSync('react'), SystemJS.newModule({'default': window.React , __useDefault: true }) );
+SystemJSJS.set(SystemJS.normalizeSync('react-dom'), SystemJS.newModule({'default': window.ReactDOM , __useDefault: true }) );
+SystemJSJS.set(SystemJS.normalizeSync('react-router-dom'), SystemJS.newModule({'default': window.ReactRouterDOM , __useDefault: true }) );
+SystemJSJS.set(SystemJS.normalizeSync('react-redux'), SystemJS.newModule({'default': window.ReactRedux , __useDefault: true }) );
+SystemJSJS.set(SystemJS.normalizeSync('redux'), SystemJS.newModule({'default': window.Redux , __useDefault: true }) );
+
 const start = () => {
   const RenderApp = require('react-sea/lib/RenderApp');
   const App = require('./App');
