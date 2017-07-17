22a522f09a3de23866608f39101e94baad808ffc
heineiuo <heineiuo@gmail.com>
2017-07-17 22:47:16 +0800

    update errors

--- src/errors.js
+++ src/errors.js
@@ -1,5 +1,20 @@
 export class NotFoundError extends Error {
   constructor(...arg) {
     super(...arg)
+    this.name = 'NotFoundError'
+  }
+}
+
+export class ValidationError extends Error {
+  constructor(...arg) {
+    super(...arg)
+    this.name = 'ValidationError'
+  }
+}
+
+export class NetworkError extends Error {
+  constructor(...arg) {
+    super(...arg)
+    this.name = 'NetworkError'
   }
 }
