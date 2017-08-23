24056fe67d1d3ed728bba948ed94d2a732b57fd3
heineiuo <heineiuo@gmail.com>
2017-08-23 11:44:08 +0800

    examples: move topic from http internal to examples

--- packages/http/src/internal/index.js
+++ packages/http/src/internal/index.js
@@ -5,26 +5,7 @@ export const actions = {
   account: require('./account').default,
   fs: require('./fs').default,
   drive: require('./drive').default,
-  client: require('./client').default,
-  socket: require('./session').default,
-  version,
-  topic: {
-    create: require('./topic/create').default,
-    edit: require('./topic/edit').default,
-    get: require('./topic/get').default,
-    render: require('./topic/render').default,
-    list: require('./topic/list').default,
-    remove: require('./topic/remove').default,
-    editTags: require('./topic/editTags').default,
-    editStatus: require('./topic/editStatus').default,
-  },
-  tags: {
-    getDriveTags: require('./tags/getDriveTags').default
-  },
-  admin: {
-    db: {
-      ql:  require('./admin/ql').default,
-      getCollectionNames: require('./admin/getCollectionNames').default
-    }
-  }
+  version
 }
+
+export const locations = []
