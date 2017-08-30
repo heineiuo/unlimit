5ff2e05a8e8645fdb554d51055d591235d286382
heineiuo <heineiuo@gmail.com>
2017-08-31 00:15:29 +0800

    split http gateway as a dependent repository

--- packages/http/src/response/json.js
+++
@@ -1,5 +0,0 @@
-export default query => (dispatch, getState) => new Promise((resolve, reject) => {
-  const { response: res } = getState()
-  res.json(query)
-  resolve()
-})
--- packages/http/src/response/redirect.js
+++
@@ -1,8 +0,0 @@
-/**
- * 重定向
- */
-export default url => (getState) => new Promise((resolve, reject) => {
-  const { response: res } = getState()
-  res.redirect(url)
-  resolve()
-})
